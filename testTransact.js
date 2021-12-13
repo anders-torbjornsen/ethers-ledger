4c762131ebb21b03b850b6da55cd5f5483515dd7
Anders Torbjørnsen <anders.torbjornsen87@gmail.com>
2021-12-13 14:42:21 +0000

    made a test transaction script fixed missing node-hid types

---
+++ testTransact.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const { ethers } = require("ethers");
+const { LedgerSigner } = require("./lib");
+
+(async function() {
+    const provider = new ethers.providers.JsonRpcProvider("http://localhost:7545");
+    const signer = new LedgerSigner(provider);
+    console.log(signer);
+    console.log(await signer.getAddress());
+    try {
+        let tx = await signer.sendTransaction({
+            to: "0xAaF147Cee92E94016e66C88355cDaE02AdD31b36",
+            value: ethers.utils.parseEther("0.001")
+        });
+        console.log(tx);
+        tx = await tx.wait();
+        console.log(tx);
+    } catch (error) {
+        console.log("ERR", error);
+    }
+})();
